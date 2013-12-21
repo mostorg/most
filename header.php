7b8b5dbf15a20f1c12dd28dfccf99b9ac512edc7
Chelsea <cmlorenz@syr.edu>
2013-12-21 11:46:45 -0500

    favicons

--- header.php
+++ header.php
@@ -18,11 +18,11 @@ $selected_set = get_option('selected_theme_set');
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title><?php wp_title('|', true, 'right'); ?></title>
         <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
-        <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/ico/favicon.png" />
-        <link rel="apple-touch-icon" sizes="144x144" href="<?php echo get_template_directory_uri(); ?>/ico/apple-touch-icon-144.png" />
-        <link rel="apple-touch-icon" sizes="114x114" href="<?php echo get_template_directory_uri(); ?>/ico/apple-touch-icon-114.png" />
-        <link rel="apple-touch-icon" sizes="72x72" href="<?php echo get_template_directory_uri(); ?>/ico/apple-touch-icon-72.png" />
-        <link rel="apple-touch-icon" href="<?php echo get_template_directory_uri(); ?>/ico/apple-touch-icon-57.png" />
+        <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/ico/moststar.ico" />
+        <link rel="apple-touch-icon" sizes="57x57" href="<?php echo get_template_directory_uri(); ?>/ico/moststar-57.png" />
+        <link rel="apple-touch-icon" sizes="72x72" href="<?php echo get_template_directory_uri(); ?>/ico/moststar-72.png" />
+        <link rel="apple-touch-icon" sizes="114x114" href="<?php echo get_template_directory_uri(); ?>/ico/moststar-114.png" />
+        <link rel="apple-touch-icon" sizes="144x144" href="<?php echo get_template_directory_uri(); ?>/ico/moststar-144.png" />
         <?php wp_head(); ?>
     </head>
     <body <?php body_class(); ?>>
