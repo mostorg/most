1b69f15bdb5240e106dde8db451f95dd28977768
cmlorenz <cmlorenz@syr.edu>
2014-01-16 18:07:22 -0500

    register custom widget

--- inc/posts-widget.php
+++ inc/posts-widget.php
@@ -2,111 +2,113 @@
 /*
 Plugin Name: MOST Posts Widget
 Plugin URI: 
-Description: Simple Featured Posts is a pratical widget that allows you to show a post list with thumbnails ordered by random or recent posts. You can also choose which post types and how many posts you want to show.
+Description: A widget that allows you to show a post list via thumbnails ordered by random or recent posts with the options of specifying post types and number of posts.
 Author: Chelsea M. P. Lorenz
 Version: 1.0
-Author URI: http://chelsealorenz.com/
+Author URI: http://chelsealorenz.me/
 */
 
 class MOST_Posts_Widget extends WP_Widget {
-	function sfpWidget() {
-		parent::__construct( 
-			'most_posts_widget', 
-			'MOST Posts Widget',
-			array( 'description' => 'Lists of posts with thumbnails ordered by random or post date.' ) 
-		);
 
-	}
-	function widget( $args, $instance ) {
+    /**
+     * Register widget with WordPress.
+     */
+    function __construct() {
+            // widget actual processes
+            parent::__construct(
+                    'most_posts_widget', // ID
+                    'MOST Posts Widget', // name
+                    array( 'description' => 'Lists of posts via thumbnails.' ) // args
+            );
+    }
+
+    /**
+     * Front-end display of widget.
+     * @param array $args Widget arguments.
+     * @param array $instance Saved values from database.
+     */
+    public function widget( $args, $instance ) {
+    	// outputs the content of the widget
 		extract($args);
 		echo $before_widget;
-		echo $before_title.$instance['title'].$after_title; ?>
-		<ul id='mpw'><?php
-			global $post;
-			$tmp_post = $post;
-			$args = array( 
-				'num_posts' => $instance['nPosts'], 
-				'order_by'=> $instance['order'], 
-				'post_type' => $instance['postType'] 
-			);
-			$myposts = get_posts( $args );
-			foreach( $myposts as $post ) : setup_postdata($post); ?>
-				<li><?php 						
-					if (has_post_thumbnail()){ //<- check if the post has a Post Thumbnail assigned to it
-						$extractUrl = wp_get_attachment_image_src( get_post_thumbnail_id(), 'thumbnail');
-						$imageUrl = $extractUrl[0];
-					} else {
-						$imageUrl = first_image();
-					}
-					echo "<a href='".get_permalink()."' title='".get_the_title()."'><img width='".$w."' alt='".the_title('','',FALSE)."'/></a>"; ?>
-					<h4><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></h4>
-				</li><?php
-			endforeach;
-			$post = $tmp_post; ?>
-		</ul><?php
+			echo $before_title.$instance['title'].$after_title; ?>
+			<ul class="mpw"><?php
+				global $post;
+				$args = array( 
+					'posts_per_page' => $instance['qty'], 
+					'orderby'=> $instance['order'], 
+					'post_type' => $instance['type'] 
+				);
+				$myposts = get_posts( $args );
+				foreach( $myposts as $post ) : setup_postdata($post); ?>
+					<li><?php 						
+						if ( has_post_thumbnail() ) {
+							$image = wp_get_attachment_image_src( get_post_thumbnail_id(), 'thumbnail');
+							$src = $image[0];
+						} else {
+							$src = get_template_directory_uri().'/img/default.png';
+						} ?>
+						<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
+							<img src="<?php echo $src; ?>" alt="<?php the_title('','',FALSE); ?>" />
+							<h4><?php the_title(); ?></h4>
+						</a>
+					</li><?php
+				endforeach; wp_reset_postdata(); ?>
+			</ul><?php
 		echo $after_widget;
 	}
-	function update( $new_instance, $old_instance ) {
-		return $new_instance;
+
+    /**
+     * Sanitize widget form values as they are saved.
+     * @param array $new_instance Values just sent to be saved.
+     * @param array $old_instance Previously saved values from database.
+     * @return array Updated safe values to be saved.
+     */
+    public function update( $new_instance, $old_instance ) {
+    	// processes widget options to be saved
+    	return $new_instance;
 	}
-	function form( $instance ) { //<- set default parameters of widget
-		if ($instance) {
+
+    /**
+     * Back-end widget form.
+     * @param array $instance Previously saved values from database.
+     */
+    public function form( $instance ) {
+    	// outputs the options form on admin
+    	if ( $instance ) {
 			$title = esc_attr($instance['title']);
-			$nPosts = esc_attr($instance['nPosts']);
+			$qty = esc_attr($instance['qty']);
 			$order = $instance['order'];
+			$type = $instance['type'];
 		} else {
-			$title = "Featured Posts";
-			$nPosts = 5;
-			$order = "rand";
+			$title = 'Featured Posts';
+			$qty = 3;
+			$order = 'rand';
+			$type = 'post';
 		} ?>
 		<p>
-			<label for="<?php echo $this->get_field_id('title');?>"><?php _e('Title:'); ?></label> 
-			<input class="widefat" id="<?php echo $this->get_field_id('title');?>" name="<?php echo $this->get_field_name('title');?>" type="text" value="<?php echo $title; ?>"/>
+			<label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:'); ?></label> 
+			<input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo $title; ?>" />
 		</p>
 		<p>
-			<label for="<?php echo $this->get_field_id('nPosts');?>"><?php _e('Number of posts to show:'); ?></label> 
-			<input class="widefat" id="<?php echo $this->get_field_id('nPosts');?>" name="<?php echo $this->get_field_name('nPosts');?>" type="text" value="<?php echo $nPosts; ?>"/>
+			<label for="<?php echo $this->get_field_id('qty'); ?>"><?php _e('Number of Posts:'); ?></label> 
+			<input class="widefat" id="<?php echo $this->get_field_id('qty');?>" name="<?php echo $this->get_field_name('qty');?>" type="text" value="<?php echo $qty; ?>"/>
 		</p>
 		<p>
-			<label for="<?php echo $this->get_field_id('order');?>"><?php _e('Order:'); ?></label> 
-			<select class="widefat" id="<?php echo $this->get_field_id('order');?>" name="<?php echo $this->get_field_name('order');?>" type="radio">
-				<option value='<?php if($order == "rand"):?>rand<?php else:?>post_date<?php endif?>'><?php if($order == "rand"):?><?php _e('Random'); ?><?php else:?><?php _e('Recent Posts'); ?><?php endif?></option>
-				<option value='<?php if($order == "rand"):?>post_date<?php else:?>rand<?php endif?>'><?php if($order == "rand"):?><?php _e('Recent Posts'); ?><?php else:?><?php _e('Random'); ?><?php endif?></option>
+			<label for="<?php echo $this->get_field_id('order'); ?>"><?php _e('Order:'); ?></label> 
+			<select class="widefat" id="<?php echo $this->get_field_id('order'); ?>" name="<?php echo $this->get_field_name('order'); ?>">
+				<option <?php echo $order=='rand' ? 'selected' : ''; ?> value="rand"><?php _e('Random'); ?></option>
+				<option <?php echo $order=='post_date' ? 'selected' : ''; ?> value="post_date"><?php _e('Recent Posts'); ?></option>
+			</select>
+		</p>
+		<p>
+			<label for="<?php echo $this->get_field_id('type'); ?>"><?php _e('Post Type:'); ?></label> 
+			<select class="widefat" id="<?php echo $this->get_field_id('type'); ?>" name="<?php echo $this->get_field_name('type'); ?>">
+				<option <?php echo $type=='post' ? 'selected' : ''; ?> value="post"><?php _e('Post'); ?></option>
+				<option <?php echo $type=='event' ? 'selected' : ''; ?> value="event"><?php _e('Event'); ?></option>
+				<option <?php echo $type=='show' ? 'selected' : ''; ?> value="show"><?php _e('Show'); ?></option>
 			</select>
 		</p><?php
 	}
-}
 
-function first_image() {
-	global $post, $posts;
-	$first_img = "";
-	ob_start();
-	ob_end_clean();
-	$output = preg_match_all('/<img.+src=[\'"]([^\'"]+)[\'"].*>/i', $post->post_content, $matches);
-	$first_img = $matches [1] [0];
-
-	if(empty($first_img)){ //<- 	Defines a default image
-		$first_img = plugin_dir_url(__FILE__)."images/default.png";
-	}
-		
-	return $first_img;
-}
-
-function imgSize($img){
-
-	if(strpos($img, "/") == 0){
-		$img = substr($img,1);
-	}
-	
-	$size = @getimagesize($img);
-	return $size;
-}
-
-add_action('wp_head', 'first_image');
-add_action('wp_head', 'imgSize');
-
-function sfpw_register() {
-	register_widget( 'sfpWidget' );
-}
- 
-add_action( 'widgets_init', 'sfpw_register' );
+} // class MOST_Posts_Widget
